004df71b0bc40627e7280e834e67d1870d89cc0d
julrusak <juliannarusak@gmail.com>
2017-09-28 21:25:39 -0400

    Diag algorithms

--- board.rb
+++ board.rb
@@ -23,22 +23,25 @@ class Board
 
 	def row_check
 		board_formatted.each do |row|
-	    return true if row.all? {|space| space == 'X' || space == 'Y'}  # return from find
+	    return true if row.all? {|space| space == 'X' || space == 'O'}  # return from find
 	  end
 	  false
 	end
 
 	def column_check
 		board_formatted.transpose.each do |column|
-	    return true if column.all? {|space| space == 'X' || space == 'Y'}  # return from find
+	    return true if column.all? {|space| space == 'X' || space == 'O'}  # return from find
 	  end
 	  false
 	end
 
 	def diag_check
 		# algorithms for finding array of correct boxes below
-		# diag_1 = (0..2).collect { |i| new_board[i][i] }
-		# diag_2 = (0..2).collect { |i| new_board.reverse[i][i] }
+		diag_1 = (0..2).collect { |i| new_board[i][i] }
+		diag_2 = (0..2).collect { |i| new_board.reverse[i][i] }
+		[diag_1, diag_2].each do |diag|
+    	return true if diag.all? {|space| space == 'X' || space == 'Y'}
+  	end
 	end
 end
 
